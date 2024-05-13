178a3b34660992b52f4115221cc624ab0a2454db
Aburaya5123 <kd1302353@st.kobedenshi.ac.jp>
2024-05-13 10:40:30 +0900

    変更

--- public/index.php
+++ public/index.php
@@ -1,2 +1,13 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>隠しフィールド</title>
+</head>
+
+<a href="hidden.html">hidden.html</a>
+<a href="hidden.php">hidden.php</a>
+
+</html>
